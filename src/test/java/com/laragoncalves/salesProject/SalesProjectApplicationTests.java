82a7b0d33ccd5507584bbe77dd97eb5342e8661a
lara <laragonsilva26122003@gmail.com>
2025-02-17 19:19:19 -0300

    first commit

---
+++ src/test/java/com/laragoncalves/salesProject/SalesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laragoncalves.salesProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
